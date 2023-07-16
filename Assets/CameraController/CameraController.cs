0f210740cc9ae4459c004d6cb217f9fcdc7186ac
Nastia <adruts@mail.ru>
2023-07-16 23:32:23 +0300

    https://github.com/anastyadru/House_unity_C.git

--- Assets/CameraController/CameraController.cs
+++ Assets/CameraController/CameraController.cs
@@ -1,16 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    public Transform target; // объект, на который смотрит камера
-    public float rotationSpeed = 1f; // скорость вращения камеры
-    public float autoRotationTime = 30f; // время одного оборота в автоматическом режиме
-
-    private bool isAutoRotating = false; // флаг автоматического вращения камеры
-    private float autoRotationTimer = 0f; // таймер автоматического вращения камеры
+    public Transform target; // Ссылка на объект дома, вокруг которого будет вращаться камера
+    public float rotationDuration = 30f; // Время, за которое камера совершит оборот
 
     void Update()
     {

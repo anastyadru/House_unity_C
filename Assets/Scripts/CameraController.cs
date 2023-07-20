4d7a0b5eea78508ed47c29626b9a9341a932bf0b
Nastia <adruts@mail.ru>
2023-07-20 22:12:05 +0300

    https://github.com/anastyadru/House_unity_C.git

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,48 +17,16 @@ public class CameraController : MonoBehaviour
     
     void Start()
     {
-        target = GameObject.Find("House").transform;
+        if (currentCameraMode == CameraMode.Automatic)
+        {
+            camera.transform.LookAt(house.transform.position);
+            camera.transform.DORotate(new Vector3(0f, -360f, 0f), cameraRotationTime, RotateMode.FastBeyond360).SetEase(Ease.Linear).SetLoops(-1);
+        }
     }
 
     void Update()
     {
-        if (isAutoRotating)
-        {
-            AutoRotate();
-        }
-        else
-        {
-            HandleMouseInput();
-        }
-    }
-
-    private void AutoRotate()
-    {
-        autoRotationTimer += Time.deltaTime;
-        float angle = autoRotationTimer / autoRotationTime * 360f;
-        transform.DORotate(new Vector3(0f, -angle, 0f), autoRotationTime, RotateMode.LocalAxisAdd).SetEase(Ease.Linear).SetLoops(-1);
-    }
-
-    private void HandleMouseInput()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * rotationSpeed;
-        float mouseY = Input.GetAxis("Mouse Y") * rotationSpeed;
-        transform.RotateAround(target.position, Vector3.up, mouseX * Time.deltaTime);
-        transform.RotateAround(target.position, transform.right, -mouseY * Time.deltaTime);
-        transform.LookAt(target);
-    }
-
-    public void StartAutoRotation()
-    {
-        isAutoRotating = true;
-        autoRotationTimer = 0f;
-        transform.DORotate(new Vector3(0f, -360f, 0f), autoRotationTime, RotateMode.LocalAxisAdd).SetEase(Ease.Linear).SetLoops(-1);
-    }
-
-    public void StopAutoRotation()
-    {
-        isAutoRotating = false;
-        transform.DOKill();
+        
     }
 
 }
